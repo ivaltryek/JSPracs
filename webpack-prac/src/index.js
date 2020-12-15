2128b87597dfc57d6ca308db9430c526ed89567f
Meet Vasani <meet.vasani86@gmail.com>
2020-12-15 19:37:05 +0530

    webpack asset management

--- webpack-prac/src/index.js
+++ webpack-prac/src/index.js
@@ -1,6 +1,7 @@
 // Basics Of Webpack -  First Step!
 // To run- npx webpack
-import _ from 'lodash';
+/* Part of the basics of Webpack start */
+/*
 
 function component() {
   const el = document.createElement('div');
@@ -8,4 +9,20 @@ function component() {
   return el;
 }
 
+document.body.appendChild(component());
+Part of the basics Webpack ends  */
+import './style.css';
+import _ from 'lodash';
+import Icon from './icon.png';
+
+function component() {
+  const el = document.createElement('div');
+  el.innerHTML = _.join(['Hello', 'Webpack']);
+  el.classList.add('hello');
+
+  const myIcon = new Image();
+  myIcon.src = Icon;
+  el.appendChild(myIcon);
+  return el;
+}
 document.body.appendChild(component());
